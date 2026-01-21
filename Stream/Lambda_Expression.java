e96bd78db390f7aa4f8f69b5e62a2bb09e2c9336
Ankita <ghadge.ankita197@gmail.com>
2026-01-21 22:32:05 +0530

    added more explanation

--- Stream/Lambda_Expression.java
+++ Stream/Lambda_Expression.java
@@ -1,17 +1,21 @@
 package Stream;
 
 public class Lambda_Expression {
-
     public static void main(String[] args) {
-
         // Traditional way
         Addition add = new Addition();
         int addRes = add.calculate(2, 3);
         System.out.println(addRes);
 
         // Lambda Expression
-        MathOperations sum = (a, b) -> a + b;
-        int sumRes = sum.calculate(3, 2);
+        // No need of parameter type, If single stmt then no need of return
+        MathOperations sum = (int a, int b) -> {
+            return a + b;
+        };
+
+        // sum1 is object that knows how to perform addition but instead of writing whole class Addition we define its behaviour 
+        MathOperations sum1 = (a, b) -> a + b;
+        int sumRes = sum1.calculate(3, 2);
         System.out.println(sumRes);
 
         MathOperations sub = (a, b) -> a - b;
