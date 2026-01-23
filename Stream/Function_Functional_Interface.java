4f75275f62aa2e61ebba1d9643b8c7149db0450e
Ankita <ghadge.ankita197@gmail.com>
2026-01-23 20:38:17 +0530

    added comments

--- Stream/Function_Functional_Interface.java
+++ Stream/Function_Functional_Interface.java
@@ -9,9 +9,20 @@ public class Function_Functional_Interface {
         Function<Integer, Integer> twice = x -> x * 2;
         Function<Integer, Integer> triple = x -> x * 3;
 
-        int result = twice.compose(triple).apply(20); // 20 * (2 * 3)
-        System.out.println(result);
+        // Parameterized function will be executed after firts one
+        int result1 = twice.andThen(triple).apply(20); // (20 * 2) * 3
+        System.out.println(result1); 
 
+        // Parameterized function will be xecuted before firts one
+        int result2 = twice.compose(triple).apply(20); // 20 * (2 * 3)
+        System.out.println(result2);
+
+        //If any function in the chain trows an exception:
+        // 1. Execution stops at that function
+        // 2. Remaining functions are not executed
+        // 3. Exception is propagated to the caller
+
+        // Returns its own argument
         Function<Integer, Integer> identity = Function.identity();
         int identityResult = identity.apply(1);
         System.out.println(identityResult);
