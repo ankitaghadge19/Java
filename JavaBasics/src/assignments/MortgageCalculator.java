a54b90fbc97927583b8adcbbf51a2866a5ab1761
Ankita <ghadge.ankita197@gmail.com>
2026-03-28 17:09:12 +0530

    refactor: create common method to read parameters

--- JavaBasics/src/assignments/MortgageCalculator.java
+++ JavaBasics/src/assignments/MortgageCalculator.java
@@ -8,23 +8,41 @@ public class MortgageCalculator {
         Scanner sc = new Scanner(System.in);
         final byte MONTHS_IN_YEAR = 12;
 
-        System.out.print("Principal: ");
-        int principal = sc.nextInt();
+        double principal = readInput(sc, 1000, 10_00_000, "Principal (₹1k - ₹1M): ");
 
-        System.out.print("Rate of Interest(Annually): ");
-        float annualInterest = sc.nextFloat();
-        float monthlyInterest = annualInterest / 100 / MONTHS_IN_YEAR;
+        double annualInterest = readInput(sc, 1, 30, "Rate of Interest(Annually): ");
+        double monthlyInterest = annualInterest / 100 / MONTHS_IN_YEAR;
 
+        double years = readInput(sc, 1, 30, "Period (Years): ");
+        int noOfPayments = ((int) years) * MONTHS_IN_YEAR;
 
-        System.out.print("Period (Years): " );
-        byte years = sc.nextByte();
-        int noOfPayments = years * MONTHS_IN_YEAR;
-
+        // M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1 ]
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, noOfPayments))
-                / (Math.pow(1 + monthlyInterest, noOfPayments));
+                / (Math.pow(1 + monthlyInterest, noOfPayments) - 1);
 
         String formatedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
         System.out.println("Mortgage: " + formatedMortgage);
+    }
+
+    // Common method to read parameters required to calculate Mortgage (Home Loan)
+    private static double readInput(Scanner sc, double min, double max, String message){
+        double finalValue = 0;
+        while(true){
+            System.out.print(message);
+            // .nextDouble() -> throws error if provided String
+            // .hasNextDouble() -> returns true if input is double else false
+            if(sc.hasNextDouble()){
+                double input = sc.nextDouble();
+                if(input >= min && input <= max){
+                    finalValue = input;
+                    break;
+                }
+            }else{
+                sc.next(); // clear invalid input from buffer
+            }
+            System.out.println("Error: Enter value between " + min + " and " +  max);
+        }
+        return finalValue;
     }
 }
